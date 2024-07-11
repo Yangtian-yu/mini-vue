a04d8426a699c772f265f3eb5860852382e3ce27
yangtianyu <535173883@qq.com>
2024-07-12 01:52:38 +0800

    1

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { effectWatch, reactive, h } from './core/index.js'
 
+window.h = h
 export default {
   render(context) {
     //ui
@@ -8,14 +9,29 @@ export default {
     // const text1 = document.createTextNode(context.obj.count);
     // element.append(text)
     // element.append(text1)
-    return h("div", { id: "foo" }, [h("p", {}, 'nihao'), h("p", {}, context.obj.count)])
-
+    // return h("div", { id: "foo" }, [h("p", {}, 'nihao'), h("p", {}, String(context.obj.count))])
+    //1.test diff tag
+    // return h(context.obj.tag, {}, "1")
+    //2.test props 1 add
+    // return h('div', context.obj.props, "")
+    //3.test props 2 remove
+    // return h("div", context.obj.props, "")
+    //test children
+    // return h("div", {}, context.obj.children)
+    // 7. test children new array -> old array
+    return h("div", {}, context.obj.children)
   },
 
 
   setup() {
     const obj = reactive({
-      count: 1
+      count: 1,
+      tag: "div",
+      props: {
+        a: "a",
+        b: "bb"
+      },
+      children: [h("div", {}, "10"), h("div", {}, "888")]
     })
     window.obj = obj
     return { obj }
--- core/createApp.js
+++ core/createApp.js
@@ -1,14 +1,29 @@
-import { effectWatch, mountElement } from './index.js'
+import { effectWatch, mountElement, diff } from './index.js'
 
 export function createApp(rootComponent) {
   return {
     mount(rootContainer) {
       const setupResult = rootComponent.setup()
+      let isOnmount = true
+      let prevSubtree = null
+
+
       effectWatch(() => {
-        rootContainer.textContent = ``
-        const subTree = rootComponent.render(setupResult)
-        console.log(subTree)
-        mountElement(subTree)
+        if (isOnmount) {
+          isOnmount = false
+          rootContainer.textContent = ``
+          const subTree = rootComponent.render(setupResult)
+          prevSubtree = subTree
+          mountElement(subTree, rootContainer)
+        } else {
+          rootContainer.textContent = ``
+          const subTree = rootComponent.render(setupResult)
+          console.log('prevSubtree', prevSubtree)
+          console.log('subTree', subTree)
+          diff(prevSubtree, subTree)
+          mountElement(subTree, rootContainer)
+        }
+
         // rootContainer.append(element)
       })
     }
