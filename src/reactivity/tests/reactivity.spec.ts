dbc57624fa1ac92e562d97c75466a756d73719fd
yangtianyu <535173883@qq.com>
2024-07-18 16:48:17 +0800

    1

---
+++ src/reactivity/tests/reactivity.spec.ts
@@ -0,0 +1,8 @@
+describe("reactive", () => {
+  it("happy path", () => {
+    const orginal = { foo: 1 };
+    const observed = reactive(orginal);
+    expect(observed).not.toBe(orginal);
+    expect(observed.foo).toBe(1);
+  });
+});
